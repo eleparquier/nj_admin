696ef0ce26046954c8387f110a759331b7d7ad74
emmanuel.leparquier <emmanuel.leparquier@wister.fr>
2015-09-06 15:45:13 +0200

    page règles

---
+++ views/pages/Admin/Regles.php
@@ -0,0 +1,3 @@
+<?php include(dirname(__FILE__)."/../../header.php"); ?>
+    <script src="<?php echo fr\gilman\nj\Conf::common()['urlAdmin']['js']; ?>/pages/Admin/regles.js"></script>
+<?php include(dirname(__FILE__)."/../../footer.php"); ?>
