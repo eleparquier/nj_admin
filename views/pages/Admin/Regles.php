da21c1b13f585f5e80da34a527b38c9017f16a7f
emmanuel.leparquier <emmanuel.leparquier@wister.fr>
2015-09-06 16:16:55 +0200

    intégration ckeditor

--- views/pages/Admin/Regles.php
+++ views/pages/Admin/Regles.php
@@ -1,5 +1,7 @@
 <?php include(dirname(__FILE__)."/../../header.php"); ?>
     <script src="<?php echo fr\gilman\nj\Conf::common()['urlAdmin']['js']; ?>/pages/Admin/regles.js"></script>
     <script src="<?php echo fr\gilman\nj\Conf::common()['urlAdmin']['js']; ?>/ckeditor/ckeditor.js"></script>
-    <textarea name="regles" id="regles" rows="10" cols="80"></textarea>
+    <button id="btEnvoyerRegles1" class="btn btn-default btEnvoyerRegles">Envoyer</button>
+    <textarea name="regles" id="regles" rows="20" cols="80"></textarea>
+    <button id="btEnvoyerRegles2" class="btn btn-default btEnvoyerRegles">Envoyer</button>
 <?php include(dirname(__FILE__)."/../../footer.php"); ?>
