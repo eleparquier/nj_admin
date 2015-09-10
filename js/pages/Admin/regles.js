d84506c46efbceeaf98c8518847c80b1045dedd2
emmanuel.leparquier <emmanuel.leparquier@wister.fr>
2015-09-10 13:21:36 +0200

    Correction bug modification règles

--- js/pages/Admin/regles.js
+++ js/pages/Admin/regles.js
@@ -1,27 +1,20 @@
 $(document).ready(function() {
     CKEDITOR.replace( 'regles' );
-
-    $('.btEnvoyerRegles').on('click', function (e) {
-        $.ajax({
-            type: "POST",
-            url: BASE_URL + '/index.php?page=Admin&action=modifRegles',
-            data: {regles:CKEDITOR.instances['regles'].getData()},
-            success: function(data) {
-                if(data.error != 0) {
-                    $('.loginPourError').addClass('has-error');
-                    $('.loginPourError .help-block').html(data.errorMsg);
-                } else {
-                    $('.loginPourError').removeClass('has-error');
-                    $('.loginPourError .help-block').html('');
-                    window.location.reload();
+    var btEnvoyerRegles = $('.btEnvoyerRegles');
+    btEnvoyerRegles.on('click', function (e) {
+        if(btEnvoyerRegles.hasClass('btn-primary')) {
+            $.ajax({
+                type: "POST",
+                url: BASE_URL + '/index.php?page=Admin&action=modifRegles',
+                data: {regles: CKEDITOR.instances['regles'].getData()},
+                success: function (data) {
+                    btEnvoyerRegles.removeClass('btn-primary');
                 }
-            },
-            dataType: 'json'
-        });
+            });
+        }
     });
 
     CKEDITOR.instances['regles'].on('key', function(e) {
-        $('.groupBt').addClass('has-warning');
-        $('.groupBt').find('.help-block').html('Le texte a été mofifié');
+        btEnvoyerRegles.addClass('btn-primary');
     });
 });
--- views/pages/Admin/Regles.php
+++ views/pages/Admin/Regles.php
@@ -2,17 +2,11 @@
     <script src="<?php echo fr\gilman\nj\Conf::common()['urlAdmin']['js']; ?>/pages/Admin/regles.js"></script>
     <script src="<?php echo fr\gilman\nj\Conf::common()['urlAdmin']['js']; ?>/ckeditor/ckeditor.js"></script>
 
-    <form class="col-sm-12">
+    <div class="row col-sm-12">
         <fieldset>
-            <div class="form-group groupBt" id="groupBt1">
-                <button id="btEnvoyerRegles1" class="btn btn-default btEnvoyerRegles">Envoyer</button>
-                <span class="help-block"></span>
-            </div>
+            <button id="btEnvoyerRegles1" class="btn btn-default btEnvoyerRegles">Envoyer</button>
             <textarea name="regles" id="regles" rows="20" cols="80"><?php echo $regles; ?></textarea>
-            <div class="form-group groupBt" id="groupBt2">
-                <button id="btEnvoyerRegles2" class="btn btn-default btEnvoyerRegles">Envoyer</button>
-                <span class="help-block"></span>
-            </div>
+            <button id="btEnvoyerRegles2" class="btn btn-default btEnvoyerRegles">Envoyer</button>
         </fieldset>
-    </form>
+    </div>
 <?php include(dirname(__FILE__)."/../../footer.php"); ?>
