a643a3c283688b186a403eba10d67e36f5dffa7f
emmanuel.leparquier <emmanuel.leparquier@wister.fr>
2015-10-09 13:13:03 +0200

    réparation de l'index cli

--- cli.php
+++ cli.php
@@ -14,9 +14,15 @@ ini_set("display_errors", 1);
 
 $action = $_SERVER['argv'][1];
 
+
 require_once(dirname(__FILE__).'/models/Conf.php');
 Conf::initCommon(dirname(__FILE__).'/common.ini');
+Conf::initPath(dirname(__FILE__).'/path.ini');
 require_once(Conf::common()['path']['commonClasses'].'/bb/inc.php');
+require_once(Conf::common()['path']['commonClasses'].'/menu/inc.php');
+require_once(Conf::common()['path']['commonClasses'].'/carte/inc.php');
+require_once(Conf::common()['path']['commonClasses'].'/rules/inc.php');
+
 require_once('controllers/Cli.php');
 DbHandler::connect(Conf::common()['DB']['HOST'],Conf::common()['DB']['DB'],Conf::common()['DB']['USER'],Conf::common()['DB']['PASS']);
 
